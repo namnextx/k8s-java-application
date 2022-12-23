6915d47341905628e449e51a508b55ff6c4bd9a5
Nam Phuong <phuongnam.nextx@gmail.com>
2022-12-23 16:33:21 +0700

    Create api get information

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,8 +1,18 @@
 package com.example.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping(value = "/test")
 public class TestController {
-// version 1
+
+  @GetMapping
+  public ResponseEntity<String> getTestResponse() {
+    String responseMessage = "Hello google cloud";
+    return ResponseEntity.ok(responseMessage);
+  }
 }
